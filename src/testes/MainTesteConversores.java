9d5a4c987655d132b79674a1ad5950b45869ae27
Leonardo Felix Scandura <Leo@MacBook-Pro-de-Leonardo.local>
2021-08-08 17:16:25 -0300

    diagrama

--- src/testes/MainTesteConversores.java
+++ src/testes/MainTesteConversores.java
@@ -9,7 +9,10 @@ public class MainTesteConversores {
 	public static void main(String[] args) {
 		
 	Imagem batman = new Imagem();
-	batman.setPath("/Users/Leo/Documents/Eclipse/lib-imgFactory/IMGTeste/cryptex.jpg");	
+	batman.setPath("/Users/Leo/Documents/Eclipse/IMGFactory/IMGTeste/cryptex.jpg");	
 	Conversor.fazerConversao(batman, "nomeDoArquivo", new BMP());
+	
+	
+	
 	}
 }
